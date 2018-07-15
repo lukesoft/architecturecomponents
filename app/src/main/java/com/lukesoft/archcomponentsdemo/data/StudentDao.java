9a1e0bf4c706fee5b70b87b3e1a61c96c69746c9
Luke Madzedze <lukemadzedze@gmail.com>
2018-07-15 11:52:04 +0200

    made dao methods more redable

--- app/src/main/java/com/lukesoft/archcomponentsdemo/data/StudentDao.java
+++ app/src/main/java/com/lukesoft/archcomponentsdemo/data/StudentDao.java
@@ -2,6 +2,7 @@ package com.lukesoft.archcomponentsdemo.data;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
@@ -15,9 +16,12 @@ public interface StudentDao {
     @Insert
     void insert(Student student);
 
-    @Query("DELETE FROM student")
-    void deleteAll();
-
     @Query("SELECT * from student ORDER BY name ASC")
     LiveData<List<Student>> getAllStudents();
+
+    @Delete
+    void delete(Student student);
+
+    @Query("SELECT * from student WHERE id = :id")
+    Student getMyFavoriteStudentById(int id);
 }
